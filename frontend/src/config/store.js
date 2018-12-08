8f4218e393c43eb67dc1ce67d6f50e05ff21d364
joeberth <joeberth.souza@ccc.ufcg.edu.br>
2018-12-08 06:09:58 -0300

    inicia front

---
+++ frontend/src/config/store.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        isMenuVisible: true,
+        user: {
+            name: 'Usuário X',
+            email: 'teste@ccc.ufcg.edu.br'
+        }
+    },
+    mutations:{
+        toggleMenu(state, isVisible) {
+            if(isVisible === undefined) {
+                state.isMenuVisible = !state.isMenuVisible
+            } else {
+                state.isMenuVisible = isVisible
+            }
+        }
+    }
+})
